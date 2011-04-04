6165995b138bfe2681e0854cd85eb2a9fbe082b4
Mike Williams <mdub@dogbiscuit.org>
2011-04-04 22:02:33 +1000

    Support transformation of values when uniq'ing.

--- lib/uniq_enumerator.rb
+++ lib/uniq_enumerator.rb
@@ -2,8 +2,9 @@ require 'set'
 
 class UniqEnumerator
   
-  def initialize(base_enum)
+  def initialize(base_enum, &transform)
     @base_enum = base_enum.to_enum
+    @transform = transform
   end
   
   include Enumerable
@@ -17,9 +18,14 @@ class UniqEnumerator
   private
   
   def seen?(value)
+    transformed_value = if @transform
+      @transform.call(value)
+    else
+      value
+    end
     @seen ||= Set.new
-    previously_seen = @seen.member?(value)
-    @seen << value
+    previously_seen = @seen.member?(transformed_value)
+    @seen << transformed_value
     previously_seen
   end
   
--- spec/uniq_enumerator_spec.rb
+++ spec/uniq_enumerator_spec.rb
@@ -2,14 +2,31 @@ require "spec_helper"
 require "uniq_enumerator"
 
 describe UniqEnumerator do
-  
-  before do
-    @array = [1,3,2,4,3,5,4,6]
-    @uniq_enum = UniqEnumerator.new(@array.to_enum)
+
+  context "without a block" do
+
+    before do
+      @array = [1,3,2,4,3,5,4,6]
+      @uniq_enum = UniqEnumerator.new(@array.to_enum)
+    end
+
+    it "removes duplicates" do
+      @uniq_enum.to_a.should == [1,3,2,4,5,6]
+    end
+
   end
 
-  it "removes duplicates" do
-    @uniq_enum.to_a.should == [1,3,2,4,5,6]
+  context "with a block" do
+
+    before do
+      @array = %w(A1 A2 B1 A3 C1 B2 C2)
+      @uniq_enum = UniqEnumerator.new(@array.to_enum) { |s| s[0,1] }
+    end
+
+    it "uses the block to derive identity" do
+      @uniq_enum.to_a.should == %w(A1 B1 C1)
+    end
+
   end
   
 end
