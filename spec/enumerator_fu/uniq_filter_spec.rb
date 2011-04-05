48f087402b3fa0bb7adfe3e509429eee63d4cf90
Mike Williams <mdub@dogbiscuit.org>
2011-04-06 07:50:05 +1000

    Check that UniqFilter is lazy.

--- spec/enumerator_fu/uniq_filter_spec.rb
+++ spec/enumerator_fu/uniq_filter_spec.rb
@@ -9,11 +9,11 @@ describe EnumeratorFu::UniqFilter do
 
     before do
       @array = [1,3,2,4,3,5,4,6]
-      @uniq_enum = UniqFilter.new(@array.to_enum)
+      @uniq = UniqFilter.new(@array)
     end
 
     it "removes duplicates" do
-      @uniq_enum.to_a.should == [1,3,2,4,5,6]
+      @uniq.to_a.should == [1,3,2,4,5,6]
     end
 
   end
@@ -22,13 +22,22 @@ describe EnumeratorFu::UniqFilter do
 
     before do
       @array = %w(A1 A2 B1 A3 C1 B2 C2)
-      @uniq_enum = UniqFilter.new(@array.to_enum) { |s| s[0,1] }
+      @uniq = UniqFilter.new(@array) { |s| s[0,1] }
     end
 
     it "uses the block to derive identity" do
-      @uniq_enum.to_a.should == %w(A1 B1 C1)
+      @uniq.to_a.should == %w(A1 B1 C1)
     end
 
+  end
+  
+  it "is lazy" do
+    @enum = FailingEnumerable.new([1,2,3])
+    @uniq = UniqFilter.new(@enum)
+    @uniq.take(3).should == [1,2,3]
+    lambda do
+      @uniq.take(4)
+    end.should raise_error("hell")
   end
   
 end
