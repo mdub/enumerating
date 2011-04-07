7c234d6af6704977c5c068d970f0eb36000a23d7
Mike Williams <mdub@dogbiscuit.org>
2011-04-07 22:23:50 +1000

    At least we're doing better than "enumerable_lz".

--- benchmarks/pipeline_bench.rb
+++ benchmarks/pipeline_bench.rb
@@ -3,14 +3,21 @@ require 'benchmark'
 $: << File.expand_path("../../lib", __FILE__)
 
 require "enumerable_fu/core_ext/enumerable"
+require "enumerable_lz"
 
 array = (1..100000).to_a
 
 Benchmark.bmbm do |bm|
   bm.report("conventional") do
-    array.select { |x| x.even? }.collect { |x| x*x }
+    @control = array.select { |x| x.even? }.collect { |x| x*x }
   end
   bm.report("enumerable_fu") do
-    array.selecting { |x| x.even? }.collecting { |x| x*x }.to_a
+    @fu = array.selecting { |x| x.even? }.collecting { |x| x*x }.to_a
+  end
+  bm.report("enumerable_lz") do
+    @lz = array.filter { |x| x.even? }.transform { |x| x*x }.to_a
   end
 end
+
+puts "enumerable_fu is busted" unless @fu == @control
+puts "enumerable_lz is busted" unless @lz == @control
