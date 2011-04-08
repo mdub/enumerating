9f649a4c30583a24fba4159932eeeed778f1a851
Mike Williams <mdub@dogbiscuit.org>
2011-04-09 08:01:16 +1000

    The "lazing" gem is comparable with enumerable_fu.

--- benchmarks/pipeline_bench.rb
+++ benchmarks/pipeline_bench.rb
@@ -2,8 +2,15 @@ require 'benchmark'
 
 $: << File.expand_path("../../lib", __FILE__)
 
-require "enumerable_fu/core_ext/enumerable"
 require "enumerable_lz"
+
+require "lazing"
+module Enumerable
+  alias :lazing_select :selecting
+  alias :lazing_collect :collecting
+end
+
+require "enumerable_fu/core_ext/enumerable"
 
 array = (1..100000).to_a
 
@@ -18,6 +25,9 @@ Benchmark.bmbm do |bm|
   end
   bm.report("enumerable_lz") do
     @lz = array.filter { |x| x.even? }.transform { |x| x*x }.take(1000)
+  end
+  bm.report("lazing") do
+    @lz = array.lazing_select { |x| x.even? }.lazing_collect { |x| x*x }.take(1000)
   end
   bm.report("conventional") do
     @control = array.select { |x| x.even? }.collect { |x| x*x }.take(1000)
