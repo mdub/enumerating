d421bca1afc03153fab26e523fbc59b8e9ad0bbe
Mike Williams <mdub@dogbiscuit.org>
2011-04-09 07:56:06 +1000

    Choose a different benchmark scenario, where lazy evaluation actually adds value.

--- benchmarks/pipeline_bench.rb
+++ benchmarks/pipeline_bench.rb
@@ -7,17 +7,19 @@ require "enumerable_lz"
 
 array = (1..100000).to_a
 
+# Test scenario:
+#  - filter out even numbers
+#  - square them
+#  - grab the first thousand
+
 Benchmark.bmbm do |bm|
-  bm.report("conventional") do
-    @control = array.select { |x| x.even? }.collect { |x| x*x }
-  end
   bm.report("enumerable_fu") do
-    @fu = array.selecting { |x| x.even? }.collecting { |x| x*x }.to_a
+    @fu = array.selecting { |x| x.even? }.collecting { |x| x*x }.take(1000)
   end
   bm.report("enumerable_lz") do
-    @lz = array.filter { |x| x.even? }.transform { |x| x*x }.to_a
+    @lz = array.filter { |x| x.even? }.transform { |x| x*x }.take(1000)
+  end
+  bm.report("conventional") do
+    @control = array.select { |x| x.even? }.collect { |x| x*x }.take(1000)
   end
 end
-
-puts "enumerable_fu is busted" unless @fu == @control
-puts "enumerable_lz is busted" unless @lz == @control
