613186b4cc10322f3fcef628e9accd7ee652b857
Mike Williams <mdub@dogbiscuit.org>
2011-04-13 19:58:44 +1000

    After some checking, it seems the Facets results were misleading.  EnumerableFu actually does pretty well.

--- benchmarks/pipeline_bench.rb
+++ benchmarks/pipeline_bench.rb
@@ -19,17 +19,39 @@ array = (1..100000).to_a
 #  - square them
 #  - grab the first thousand
 
-Benchmark.bmbm do |bm|
-  bm.report("enumerable_fu") do
-    @fu = array.selecting { |x| x.even? }.collecting { |x| x*x }.take(1000)
+printf "%-30s", "IMPLEMENTATION"
+printf "%12s", "take(10)"
+printf "%12s", "take(100)"
+printf "%12s", "take(1000)"
+printf "%12s", "to_a"
+puts ""
+
+def benchmark(description, control_result = nil)
+  result = nil
+  printf "%-30s", description
+  printf "%12.5f", Benchmark.realtime { yield.take(10).to_a }
+  printf "%12.5f", Benchmark.realtime { yield.take(100).to_a }
+  printf "%12.5f", Benchmark.realtime { result = yield.take(1000).to_a }
+  printf "%12.5f", Benchmark.realtime { yield.to_a }
+  puts ""
+  unless control_result.nil? || result == control_result
+    raise "unexpected result from '#{description}'"
   end
-  bm.report("lazing") do
-    @lz = array.lazing_select { |x| x.even? }.lazing_collect { |x| x*x }.take(1000)
-  end
-  bm.report("facets") do
-    @facets = array.defer.select { |x| x.even? }.collect { |x| x*x }.take(1000)
-  end
-  bm.report("conventional") do
-    @control = array.select { |x| x.even? }.collect { |x| x*x }.take(1000)
-  end
+  result
+end
+
+@control = benchmark "conventional" do
+  array.select { |x| x.even? }.collect { |x| x*x }
+end
+
+benchmark "enumerable_fu", @control do 
+  array.selecting { |x| x.even? }.collecting { |x| x*x }
+end
+
+benchmark "lazing", @control do
+  array.lazing_select { |x| x.even? }.lazing_collect { |x| x*x }
+end
+
+benchmark "facets Enumerable#defer", @control do
+  array.defer.select { |x| x.even? }.collect { |x| x*x }
 end
